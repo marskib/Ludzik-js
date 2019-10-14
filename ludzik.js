3e214d3597cc7cd924baa3e4a6b7d304cd68ae69
marskib <tata.marcina@autyzmsoft.pl>
2019-10-14 20:48:27 +0200

    dzwiek i dodanie javascript'

---
+++ ludzik.js
@@ -0,0 +1,37 @@
+"use strict";
+let handleKlikOnCzlonek = function (event) {
+    var nazwa = event.target.id.toString();
+    var nazwaOK = oczyscNazwe(nazwa); //lreka -> reka itd...
+    odegrajNazwe(nazwaOK, 500);
+    kolorEfekt(event.target, 700);
+}
+
+function oczyscNazwe(nazwa) {
+/* z lreka,preka,lnoga,pnoga robi reka, noga  */    
+    if (nazwa=="glowa"||nazwa=="szyja"||nazwa=="brzuch") {
+        return nazwa
+    };
+   return(nazwa.substring(1));
+}
+
+function odegrajNazwe(nazwa,delay) {
+    var plik = "snd/" + nazwa + ".ogg"; 
+    var nazwaSound = new Audio(plik);
+    setTimeout(() => nazwaSound.play(), delay);
+}
+
+function kolorEfekt(elem, delay) {
+/* Na chwile zmiana koloru */    
+    var kolorOrig = elem.style.backgroundColor;
+    elem.style.backgroundColor = "maroon";
+    setTimeout(()=>elem.style.backgroundColor=kolorOrig, delay);
+}
+
+let czlonki = document.querySelectorAll('.czlonek');
+[...czlonki].forEach(czlonek => czlonek.onclick = handleKlikOnCzlonek);
+//Glowa nie dostala klasy '.czlonek', wiec minimalnie inne potraktowanie:
+let glowa = document.getElementById("glowa");
+glowa.onclick = handleKlikOnCzlonek;
+
+
+
